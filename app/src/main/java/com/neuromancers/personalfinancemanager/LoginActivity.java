acae7e8792f343ab83db4dcabc20dd1dd7c2cac8
CyberPokemon <imoncoding@gmail.com>
2025-04-12 11:00:31 +0530

    Login and registration pg layout added

---
+++ app/src/main/java/com/neuromancers/personalfinancemanager/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.neuromancers.personalfinancemanager;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+    private static final String PREF_NAME = "FinanceAppPrefs";
+    private static final String KEY_IS_REGISTERED = "is_registered";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
--- app/src/main/java/com/neuromancers/personalfinancemanager/MainActivity.java
+++ app/src/main/java/com/neuromancers/personalfinancemanager/MainActivity.java
@@ -1,5 +1,7 @@
 package com.neuromancers.personalfinancemanager;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,16 +11,32 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
+    private static final String PREF_NAME = "FinanceAppPrefs";
+    private static final String KEY_IS_REGISTERED = "is_registered";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_main);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+        sharedPreferences = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+        boolean isRegistered = sharedPreferences.getBoolean(KEY_IS_REGISTERED, false);
+
+        if (isRegistered) {
+            // User is registered -> Go to Login
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        } else {
+            // First time -> Go to Registration
+            startActivity(new Intent(MainActivity.this, RegistrationActivity1.class));
+        }
+
+        finish();
     }
 }
